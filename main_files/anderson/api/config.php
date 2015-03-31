f1548b0c2496506d6186c46e293a3fb37dc6cdbd
mflindo <michaellindo@gmail.com>
2015-03-30 22:24:55 -0500

    tastypanama

---
+++ main_files/anderson/api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'Vj2EFuoaE7AoKpo1gaixDw');
+    define('CONSUMER_SECRET', '4aE1ECfs1P4Ngfs6DL9TFDn9C6aO4sZNf6yz82KuuBo');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '434475519-geP69Nq9O3IDUplbX1JHtrtl9Rz7RxSjaijWQTgZ');
+    define('ACCESS_SECRET', 'gfvg1h1mIXQsqPyqHBmL6PThZIP2OfV1wCAia6aroZM');
